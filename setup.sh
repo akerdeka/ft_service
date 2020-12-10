2492f3c9d5be04600557fa26918138483f3b6732
akerdeka <kerdea@yahoo.fr>
2020-12-10 17:48:41 +0100

    phpmyadmin marche presque

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 minikube start --vm-driver=virtualbox
 
 
+eval $(minikube docker-env)
 docker build -t services-nginx ./srcs/nginx
 docker build -t services-wordpress ./srcs/wordpress
+docker build -t services-mysql ./srcs/mysql
+docker build -t services-phpmyadmin ./srcs/phpmyadmin
 eval $(minikube docker-env)
 
 kubectl delete all --all
 kubectl create -f srcs/nginx/nginx.yaml
 kubectl create -f srcs/wordpress/wordpress.yaml
+kubectl create -f srcs/mysql/mysql.yaml
+kubectl create -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl create -f srcs/metallb.yaml
 
 minikube dashboard
--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -5,29 +5,3 @@ php -S 0.0.0.0:5050 -t /www/
 while true; do
     sleep 1;
 done
-
-
-
-
-
-##!/bin/sh
-#openrc
-#touch /run/openrc/softlevel
-#service php-fpm7 restart
-#/usr/sbin/sshd
-#nginx -g 'pid /tmp/nginx.pid; daemon off;'
-#php-fpm7
-
-#while :
-#do
-#    mysqladmin -h mysql ping
-#    if [ $? == 0 ]
-#    then
-#        break
-#    fi
-#    sleep 10
-#done
-#echo "Connection with Mysql established."
-#
-#su -c "/tmp/wpinstall.sh" - www
-#nginx -g "daemon off;"
